92a30281fe12fadcdee799ca8c99889c1ef56e8c
CostenarEduardgit config --global user.name CostenarEduardclear <costenar.eduard@gmail.com>
2020-03-13 09:18:02 +0200

    Updates

---
+++ precache-manifest.b47cfe691aed6524f907054156cebeae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05e58bd10d4116d0ff3de4200ce57aa",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "536b426da178f6740b96",
+    "url": "/react-robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b89f8b36f0adb896c2af",
+    "url": "/react-robofriends/static/css/main.9284bb86.chunk.css"
+  },
+  {
+    "revision": "536b426da178f6740b96",
+    "url": "/react-robofriends/static/js/2.a46dfb62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-robofriends/static/js/2.a46dfb62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89f8b36f0adb896c2af",
+    "url": "/react-robofriends/static/js/main.3ec02ae9.chunk.js"
+  },
+  {
+    "revision": "e2a197a5e419843132ea",
+    "url": "/react-robofriends/static/js/runtime-main.c1939bc1.js"
+  }
+]);
